8d282dff725036c6563e36e63c42c4b8da0b521a
Zelow <83988054+zeloww@users.noreply.github.com>
2021-10-10 14:31:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -31,6 +31,19 @@ async def on_ready():
     print(f"In {len(bot.guilds)} guilds")
     print(f"On {len(bot.users)} users")
 
+@bot.command()
+async def help(ctx):
+    embed = discord.Embed(title="Help", desciption=f"My prefix is {prefix}", color=discord.Color.purple())
+    embed.set_thumbnail(url=str(ctx.guild.icon_url))
+    embed.add_field(name=prefix + "youtubetogether", value="play at youtube!", inline=False)
+    embed.add_field(name=prefix + "pokertogether", value="play at poker game!", inline=False)
+    embed.add_field(name=prefix + "chesstogether", value="play at chess game!", inline=False)
+    embed.add_field(name=prefix + "betrayaltogether", value="play at betrayal game!", inline=False)
+    embed.add_field(name=prefix + "fishingtogether", value="play at fishing game!", inline=False)
+    embed.set_footer(text=bot.user)
+    embed.timestamp = datetime.datetime.utcnow()
+    await ctx.send(embed=embed)
+    
 @bot.command(aliases=['git'])
 async def github(ctx):
     embed=discord.Embed(title='GitHub', description='[Click Here](https://github.com/zeloww)', color=discord.Color.purple())
